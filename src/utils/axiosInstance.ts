0abefdb4b2dfd34496486489dd3abe12d3b39ecc
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2024-04-09 00:26:13 -0300

    Update axiosInstance.ts

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 console.log(process.env.BASE_API_URL);
 
 const axiosInstance = axios.create({
-  baseURL: "https://celtucapiinter.onrender.com"
+  baseURL: "https://celtucapiinter.onrender.com/api"
 });
 
 export default axiosInstance;
