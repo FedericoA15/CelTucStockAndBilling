f0e731ead9f8f58cfc9b750dc02dcb624493496d
FedericoA15 <federicoalmeida15@gmail.com>
2025-03-28 15:05:44 -0300

    fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import ListComponent from "./ui/ListItems";
-import TimeSheet from "@/components/timeSheet/TimeSheet";
 
 export const metadata: Metadata = {
   title: "Productos",
@@ -10,7 +9,6 @@ export default function productPage() {
   return (
     <div className="bg-custom-bg2  bg-cover bg-center bg-no-repeat min-h-screen">
       <h2 className="text-white text-5xl text-center py-10">Productos</h2>
-      <TimeSheet />
       <ListComponent />
     </div>
   );
